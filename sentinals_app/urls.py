04192811e0e3cd1d36e9314b7b43ff34555437ed
Chris <Irishmedic250@gmail.com>
2022-04-09 15:31:14 -0400

    Finishing for deployment

--- sentinals_app/urls.py
+++ sentinals_app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.landing),
+    path('home', views.index),
     path('roster', views.roster),
     path('schedule', views.schedule),
 ]
--- sentinals_app/views.py
+++ sentinals_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 
+def landing(request):
+    return render(request, 'landing.html')
+
 def index(request):
     return render(request, 'index.html')
 
